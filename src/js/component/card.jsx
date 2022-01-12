126fb660fbc4ba83ee9a48d5b548da451e0eb194
Bruno Marques <besmarques@gmail.com>
2022-01-12 20:56:52 +0000

    99 done

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-const Card = () => {
-	return <div>Card</div>;
+/*const cardInfo = [
+	{
+		Image: "./img/1jpg",
+		Title: "Card 1",
+		Text: "Lorem Ipsum 1",
+		ButtonLabel: "Go to 1",
+		ButtonUrl: "",
+	},
+	{
+		Image: "./img/2jpg",
+		Title: "Card 2",
+		Text: "Lorem Ipsum 2",
+		ButtonLabel: "Go to 2",
+		ButtonUrl: "",
+	},
+	{
+		Image: "./img/3jpg",
+		Title: "Card 3",
+		Text: "Lorem Ipsum 3",
+		ButtonLabel: "Go to 3",
+		ButtonUrl: "",
+	},
+	{
+		Image: "./img/4jpg",
+		Title: "Card 4",
+		Text: "Lorem Ipsum 4",
+		ButtonLabel: "Go to 4",
+		ButtonUrl: "",
+	},
+];*/
+
+const CardMine = (props) => {
+	return (
+		<>
+			<Card style={{ width: "18rem" }}>
+				<Card.Img variant="top" src={props.imageUrl} />
+				<Card.Body>
+					<Card.Title>{props.title}</Card.Title>
+					<Card.Text>{props.description}</Card.Text>
+					<Button variant="primary">{props.buttonLabel}</Button>
+				</Card.Body>
+			</Card>
+		</>
+	);
 };
 
-export default Card;
+export default CardMine;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,65 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import c1Image from "../../img/1.jpg";
+import c2Image from "../../img/2.jpg";
+import c3Image from "../../img/3.jpg";
+import c4Image from "../../img/4.jpg";
 
-import Navbar from "./navbar.jsx";
+import NavBar from "./navbar.jsx";
 import JumboTron from "./jumbotron.jsx";
-import Card from "./card.jsx";
+import CardMine from "./card.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<Navbar />
-			<JumboTron />
-			<Card />
-		</div>
+		<>
+			<Container>
+				<Row>
+					<Col>
+						<NavBar />
+						<JumboTron />
+						<Container>
+							<Row>
+								<Col>
+									<CardMine
+										title="Beach"
+										description="This is a great beach"
+										imageUrl={c1Image}
+										buttonLabel="See this beach"
+									/>
+								</Col>
+								<Col>
+									<CardMine
+										title="Reflection"
+										description="Reflection on the window"
+										imageUrl={c2Image}
+										buttonLabel="How to capture a reflection"
+									/>
+								</Col>
+								<Col>
+									<CardMine
+										title="Calm"
+										description="Feel calm"
+										imageUrl={c3Image}
+										buttonLabel="Learn how to calm yourself"
+									/>
+								</Col>
+								<Col>
+									<CardMine
+										title="Sea"
+										description="Sailing the sees"
+										imageUrl={c4Image}
+										buttonLabel="Sail now"
+									/>
+								</Col>
+							</Row>
+						</Container>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 
